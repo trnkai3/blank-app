2d6e737d963684c6a1374fe512699194f3052678
Kai Trentini <trnkai001@myuct.ac.za>
2025-05-22 09:09:07 +0000

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+name = st.text_input("Enter your name")
+
+studymethod = st.selectbox("Study method", ("Crammer", "Memory Box", "Pre-planner"))
+
+button = st.button("Go")
+
+if button:
+    with st.spinner("Loading your results"):
+        if studymethod == "Crammer":
+            st.write(f"{name}, You are NOT a diva")
+        elif studymethod == "Pre-planner":
+            st.write(f"{name}, You are a diva")
+        elif studymethod == "Memory Box":
+            st.write(f"{name}, You are almost a diva")
+
